f87c9bb4a9e519f3af99edc0bb84f9920170562c
nasimchapaipoly <nasim.chapaipoly@gmail.com>
2026-04-16 23:45:03 +0600

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?php
+echo '
+<footer>
+    <div class="footer-grid">
+        <div class="footer-col">
+            <h3>Food Tech CPI</h3>
+            <p>Empowering future engineers.</p>
+        </div>
+    </div>
+
+    <div class="copyright">
+        © <span id="year"></span> All Rights Reserved
+    </div>
+</footer>
+';
+?>
